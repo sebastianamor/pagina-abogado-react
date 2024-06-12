4616c5250781e34fa4b8a1bdbd55e960fb7e04b8
¨Jane <smoreira11@hotmail.com>
2024-06-12 22:58:25 +0900

    ready info

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Home.css';
+import profileImag from './imagenes/toledo.jpg'; // Asegúrate de tener una imagen en esta ruta
 
 function Home() {
   return (
     <section className="home">
-      <h2>Inicio</h2>
+      <div className="home-content">
+        <img src={profileImag} alt="Perfil" className="profile-imag" />
+        <div className="home-text">
+        <h2>Inicio</h2>
       <p>
         Somos un Estudio Jurídico especialista principalmente en el Área de Derecho Penal, pero contamos
         con diversos asociados especializados en Familia e Infancia, Responsabilidad Penal Adolescente,
@@ -16,6 +20,34 @@ function Home() {
         Sociales y Psicólogos, con el fin de atender de manera eficaz y personalizada los requerimientos de
         cada uno de nuestros clientes.
       </p>
+      <hr></hr>
+      <div><h2>Defensor Penal</h2>
+      <h1>Abogado Miguel Toledo</h1></div>
+          <ul>
+          <li>Defensor Penal</li>
+          <li>Abogado Colegiado</li>
+
+<li>Director Académico del Centro de Capacitación "Academia Internacional de Litigación"</li>
+<li>Curso internacional de litigación sobre técnicas avanzadas del contraexamne,  California Western School Of Law, de EE.UU.</li>
+<li>Certificado por la alianza interamericana de Abogacía oral de EE.UU.</li>
+<li>Curso Criminalistica, Certificado por Criminalistic and Applied Criminology International Academy del Perú.</li>
+<li>Certificación de la Asociación Chile de Seguridad (ACHS) como relator de Responsabilidad Legal.</li>
+<li>Diplomado de Litigación Derecho de Familia de la Pontificia Universidad Católica de Chile.</li>
+<li>Diplomado de Litigación de Derecho Penal de la Pontificia Universidad Católica de Chile.</li>
+<li>Diplomado de Litigación Delitos Tributarios de la Pontifica Universidad Católica de Chile.</li>
+<li>Taller Internacional de Destreza en Litigación Oral, de la Alianza Interamericana de Abogacía Oral de EE.UU.</li>
+<li>Taller de Preparación de los Testigos, de la Alianza Interamericana de Abogacía Oral de EE.UU.</li>
+<li>Taller de la Teroría del Caso de la Alianza Interamericana de Abogacía Oral de EE.UU.</li>
+<li>Taller de Habilidades a la Hora de Litigar, de la Alianza Interamericana de Abogacía Oral EE.UU.</li>
+<li>Diplomado Derecho Penal Universidad Pompeu Fabra, De España.</li>
+<li>Diplomado Abuso Sexual y Violencia Habitual Infante Juvenil U.A.</li>
+<li>C. Magister En Derecho Penal Universidad de Talca.</li>
+<li>Magister en Ciencias Penales Universidad Pompe Fabra, De España.</li>
+          </ul>
+          <p>
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
--- src/Services.js
+++ src/Services.js
@@ -1,16 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Services.css';
 
-function Services({onHomeClick}){
+function Services() {
+  const [message, setMessage] = useState('');
+
+  const handleButtonClick = (service) => {
+    let msg = '';
+    switch (service) {
+      case 'asesoria':
+      msg = <div><p><h1>Derecho Penal</h1></p>
+            <p>Dppj se especializa en litigios de alta complejidad a nivel nacional e internacional. El estudio
+               otorga asesoría especializada en delitos económicos, tributarios, violaciones a leyes anti-
+               corrupción, lavado de activos, e infracciones a leyes de propiedad industrial e intelectual, entre
+               otros. </p>
+               <p>
+               contamos con un equipo penal multidisciplinario, lo que le permite representar a imputados y
+               víctimas, desde una etapa temprana, en materia de prevención y análisis de riesgos, así como
+               con una adecuada planificación, proceso de investigación interna y reacción estratégica frente
+               a una contingencia penal, representando los intereses de los clientes ante instancias
+               administrativas y judiciales a lo largo de todo el territorio nacional. </p></div>
+            
+        break;
+      case 'representacion':
+        msg = <div><p><h1>Derecho Civil</h1></p>
+               <p>Civil, brindamos un completo apoyo a demandantes o demandados en sus causas,
+                proporcionando una solución rápida y oportuna a sus problemas y necesidades.
+                Juicios de Arrendamiento </p>
+                <ul>
+                  <li>Prescripción de Deudas</li>
+                  <li>Precarios
+                  Cambio de nombre</li>
+                  <li>Cobro de Honorarios</li>
+                  <li>Indemnización de Perjuicios
+                      Responsabilidad Extracontractual</li>
+                      <li>Asesoría Inmobiliaria</li>
+                  </ul></div>
+        break;
+      case 'consultoria':
+        msg = 'Consultoría especializada en derecho corporativo';
+        break;
+      case 'redaccion':
+        msg = <div><p><h1>Derecho Laboral</h1></p>
+               <p>En materia sobre derecho laboral, contamos con amplia experiencia en procesos de
+                  contratación y terminación de trabajadores (individuales y masivas), paquetes de
+                  compensación (incluyendo planes de acciones) cumplimiento legal, subcontrataciones,
+                  políticas corporativas, asuntos laborales derivados y/o asociados a fusiones y adquisiciones,
+                  investigaciones internas, asuntos de seguridad social e impuestos a los trabajadores.
+                  participamos activamente en procesos de negociación colectiva en empresas de diferentes
+                  rubros y también asesora a clientes en juicios y reclamaciones laborales, incluyendo litigios
+                  y procedimientos administrativos derivados de accidentes laborales y enfermedades 
+                  profesionales.
+                  En materia de subcontratación de servicios, asesoramos permanentemente a empresas
+                  subcontratistas y propietarios en proyectos relativos a estas materias, identificando y
+                  mitigando riesgos y responsabilidades legales asociadas.</p>
+                  <ul>
+                    <li>Asesoría Laboral a Empresas</li>
+                    <li>cobranza laboral y previsional</li>
+                    <li>Abogados Trabajadores</li>
+                    <li>Despido injustificado</li>
+                    <li>Accidente laboral y enfermedad Profesional</li>
+                    <li>Tutela Laboral</li></ul></div>
+        break;
+      default:
+        msg = '';
+    }
+    setMessage(msg);
+  };
+
   return (
     <section className="services">
       <h2>Servicios</h2>
-      <div className="services-buttons">
-        <button>Asesoría legal</button>
-        <button>Representación en juicios</button>
-        <button>Consultoría en derecho corporativo</button>
-        <button>Redacción de contratos</button>
+      <div className="service-buttons">
+        <button onClick={() => handleButtonClick('asesoria')}>Derecho Penal</button>
+        <button onClick={() => handleButtonClick('representacion')}>Derecho Civil</button>
+        <button onClick={() => handleButtonClick('consultoria')}>Derecho de Familia</button>
+        <button onClick={() => handleButtonClick('redaccion')}>Derecho Laboral</button>
       </div>
+      {message && <p className="service-message">{message}</p>}
     </section>
   );
 }
